c031526cd241d9057a07c0082178fabfd524e1cc
Rayan Roshan <2006rayanroshan@gmail.com>
2025-07-15 19:30:58 -0400

    Fixed chat text color

--- src/components/VapiWidget.tsx
+++ src/components/VapiWidget.tsx
@@ -340,7 +340,7 @@ const VapiWidget: React.FC<VapiWidgetProps> = ({ apiKey, assistantId }) => {
               onChange={(e) => setCurrentMessage(e.target.value)}
               onKeyPress={handleKeyPress}
               placeholder="Type your message..."
-              className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black"
             />
             <button
               onClick={sendTextMessage}
